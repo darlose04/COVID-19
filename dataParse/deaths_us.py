f41741047c21b9534d5566ac241f392462fd38ab
Zach Smith <zsmith126028@gmail.com>
2020-04-22 16:29:25 -0500

    import datetime. trying different ways of playing with data with pandas

--- dataParse/deaths_us.py
+++ dataParse/deaths_us.py
@@ -1,9 +1,14 @@
 import pandas as pd
+import datetime
 
 csv_path = '/home/zach/Storage/Projects/COVID-19/covidData/csse_covid_19_data/csse_covid_19_time_series/'
 
 us_deaths = pd.read_csv(csv_path + 'time_series_covid19_deaths_US.csv')
 
+yesterday = us_deaths["4/20/20"]
 today = us_deaths["4/21/20"]
+first_date = "1/22/20"
 
-print(today)
+print(datetime.datetime.now())
+
+print(today.sum() - yesterday.sum())
