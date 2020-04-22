0e1cacdb00bc6bd7a0313158fdcf19a43f0d3881
Zach Smith <zsmith126028@gmail.com>
2020-04-22 16:11:42 -0500

    add full path to variable

--- dataParse/deaths_us.py
+++ dataParse/deaths_us.py
@@ -1,6 +1,6 @@
-import pandas
+import pandas as pd
 
-csv_path = '../covidData/csse_covid_19_data/csse_covid_19_time_series/'
+csv_path = '/home/zach/Storage/Projects/COVID-19/covidData/csse_covid_19_data/csse_covid_19_time_series/'
 
-df = pandas.read_csv(csv_path + 'time_series_covid19_deaths_US.csv')
-print(df)
+df = pd.read_csv(csv_path + 'time_series_covid19_deaths_US.csv')
+print(df.head(20))
