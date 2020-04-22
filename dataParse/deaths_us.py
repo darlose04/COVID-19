6f7ff52d7e6772880eef9c0c2166ea7c3e6b6238
Zach Smith <zsmith126028@gmail.com>
2020-04-22 16:02:53 -0500

    install pandas to parse csv

--- dataParse/deaths_us.py
+++ dataParse/deaths_us.py
@@ -1,2 +1,6 @@
-import csv
+import pandas
 
+csv_path = '../covidData/csse_covid_19_data/csse_covid_19_time_series/'
+
+df = pandas.read_csv(csv_path + 'time_series_covid19_deaths_US.csv')
+print(df)
