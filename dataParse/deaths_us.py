04d242664803e42bd5ded7e57135f983f9da5734
Zach Smith <zsmith126028@gmail.com>
2020-04-22 16:21:40 -0500

    playing around with different ways of getting data from csv with pandas

--- dataParse/deaths_us.py
+++ dataParse/deaths_us.py
@@ -2,5 +2,8 @@ import pandas as pd
 
 csv_path = '/home/zach/Storage/Projects/COVID-19/covidData/csse_covid_19_data/csse_covid_19_time_series/'
 
-df = pd.read_csv(csv_path + 'time_series_covid19_deaths_US.csv')
-print(df.head(20))
+us_deaths = pd.read_csv(csv_path + 'time_series_covid19_deaths_US.csv')
+
+today = us_deaths["4/21/20"]
+
+print(today)
