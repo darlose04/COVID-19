33ef591eeb818e2600ef4f30004357b8ae43b507
Zach Smith <zsmith126028@gmail.com>
2020-04-24 17:10:53 -0500

    create variables for each column that I want to push into a database. write comments for what type of data each column holds

--- dataParse/deaths_us.py
+++ dataParse/deaths_us.py
@@ -9,11 +9,20 @@ us_deaths = pd.read_csv(csv_path + 'time_series_covid19_deaths_US.csv')
 # today = us_deaths["4/21/20"]
 # first_date = "1/22/20"
 
-dates = us_deaths.columns[12:]
+# Make variables for each column from the csv that I want to push into SQL
 
-for item in dates:
-  print(item, us_deaths[item].sum())
+uid = us_deaths.columns[0] # int or float
+city = us_deaths.columns[5]  # string. will need to account for null values
+province_state = us_deaths.columns[6] # string
+latitude = us_deaths.columns[8] # float
+longitude = us_deaths.columns[9] # float
+population = us_deaths.columns[11] # int
+dates = us_deaths.columns[12:] # ints
 
-uid = us_deaths.columns[0]
+# for item in dates:
+#   print(item, us_deaths[item].sum())
 
-print(us_deaths[uid])
+# print(type(us_deaths[latitude]))
+
+for item in us_deaths[dates[-1]]:
+  print(type(item))
