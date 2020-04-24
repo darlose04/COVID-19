513640c493e5a5d8fb231a5b5ded0182e11150c4
Zach Smith <zsmith126028@gmail.com>
2020-04-24 16:41:59 -0500

    bash scriipts ran which changed the output files. Could probably put those in .gitignore. still messing with pandas and grabbing different data

--- dataParse/deaths_us.py
+++ dataParse/deaths_us.py
@@ -5,19 +5,19 @@ csv_path = '/home/zach/Storage/Projects/COVID-19/covidData/csse_covid_19_data/cs
 
 us_deaths = pd.read_csv(csv_path + 'time_series_covid19_deaths_US.csv')
 
-yesterday = us_deaths["4/20/20"]
-today = us_deaths["4/21/20"]
-first_date = "1/22/20"
+# yesterday = us_deaths["4/20/20"]
+# today = us_deaths["4/21/20"]
+# first_date = "1/22/20"
 
 # print(datetime.datetime.now())
 
 # print(today.sum() - yesterday.sum())
 
-# for col in us_deaths:
-#   print(col)
-
 dates = us_deaths.columns[12:]
 
+for item in dates:
+  print(item, us_deaths[item].sum())
 
-print(dates[-1])
-print(us_deaths[dates[-1]].sum())
+uid = us_deaths.columns[0]
+
+print(us_deaths[uid])
