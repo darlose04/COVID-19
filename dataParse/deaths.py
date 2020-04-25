34fc701eedc37b5c382113be4659af3384d7d9ea
Zach Smith <zsmith126028@gmail.com>
2020-04-25 10:13:36 -0500

    add mysql connection

--- dataParse/deaths.py
+++ dataParse/deaths.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import mysql.connector
+import os
 
 csv_path = '/home/zach/Storage/Projects/COVID-19/covidData/csse_covid_19_data/csse_covid_19_time_series/'
 
@@ -6,3 +8,15 @@ us_deaths = pd.read_csv(csv_path + 'time_series_covid19_deaths_US.csv')
 
 uid = us_deaths.columns[0]
 dates = us_deaths.columns[12:]
+
+# print(us_deaths[dates[-1]].sum())
+
+# Create database connection
+mydb = mysql.connector.connect(
+  host='localhost',
+  user='zach',
+  passwd=os.environ['DBPASSWD'],
+  database='covid19'
+)
+
+print(mydb)
