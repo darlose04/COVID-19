e07c75002c17c0b7fb723e16fd74166c428454b7
Zach Smith <zsmith126028@gmail.com>
2020-04-25 14:51:53 -0500

    add if else statements to change the format of the date

--- dataParse/deaths.py
+++ dataParse/deaths.py
@@ -21,3 +21,37 @@ mycursor = mydb.cursor()
 # mycursor.execute("CREATE TABLE deaths (UID INT AUTO_INCREMENT PRIMARY KEY)")
 
 # mycursor.execute(f"ALTER TABLE deaths ADD COLUMN FIRST INT AFTER UID")
+
+for item in dates:
+  date_split = item.split("/")
+  month = date_split[0]
+  day = date_split[1]
+  year = date_split[2]
+
+  if month == "1":
+    month = "Jan"
+  elif month == "2":
+    month = "Feb"
+  elif month == "3":
+    month = "Mar"
+  elif month == "4":
+    month = "Apr"
+  elif month == "5":
+    month = "May"
+  elif month == "6":
+    month = "Jun"
+  elif month == "7":
+    month = "Jul"
+  elif month == "8":
+    month = "Aug"
+  elif month == "9":
+    month = "Sep"
+  elif month == "10":
+    month = "Oct"
+  elif month == "11":
+    month = "Nov"
+  elif month == "12":
+    month = "Dec"
+
+  date = month + day + year
+  print(date)
