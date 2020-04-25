32d77a2cf6e25d5e9bc9c8424538f02c4ba0ad08
Zach Smith <zsmith126028@gmail.com>
2020-04-25 10:59:32 -0500

    import os, connect to db. change uid values to ints

--- dataParse/usa_data.py
+++ dataParse/usa_data.py
@@ -1,9 +1,17 @@
 import pandas as pd
-import datetime
+import mysql.connector
+import os
 
 csv_path = '/home/zach/Storage/Projects/COVID-19/covidData/csse_covid_19_data/csse_covid_19_time_series/'
 
 us_deaths = pd.read_csv(csv_path + 'time_series_covid19_deaths_US.csv')
+
+mydb = mysql.connector.connect(
+  host='localhost',
+  user='zach',
+  passwd=os.environ['DBPASSWD'],
+  database='covid19'
+)
 
 # yesterday = us_deaths["4/20/20"]
 # today = us_deaths["4/21/20"]
@@ -17,20 +25,17 @@ province_state = us_deaths.columns[6] # string
 latitude = us_deaths.columns[8] # float
 longitude = us_deaths.columns[9] # float
 population = us_deaths.columns[11] # int
-dates = us_deaths.columns[12:] # ints
+#dates = us_deaths.columns[12:] # ints
 
-# for item in dates:
-#   print(item, us_deaths[item].sum())
+# want to grab every item in uid column except for last row since last row is NaN
+uid_vals = us_deaths[uid][:-1]
 
-# print(type(us_deaths[latitude]))
+for num in uid_vals:
+  num = int(num)
+  print(num)
 
-for item in us_deaths[uid]:
-  # the UID column is made up of floats for some reason, even though they appear to be ints in the csv
-  # probably should convert them to ints before putting them into SQL
-  # print(type(int(item)))
-  # print(item)
-  # if int(item) == 84001000:
-  #   print(item)
+
+
 
 # for item in us_deaths[latitude]:
 #   if item > 100:
