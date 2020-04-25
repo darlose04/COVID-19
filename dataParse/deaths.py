fa2d019a760f9f4f8ecab524d240dbe398a5ff57
Zach Smith <zsmith126028@gmail.com>
2020-04-24 19:04:26 -0500

    add file to parse deaths and add them to sql

---
+++ dataParse/deaths.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+csv_path = '/home/zach/Storage/Projects/COVID-19/covidData/csse_covid_19_data/csse_covid_19_time_series/'
+
+us_deaths = pd.read_csv(csv_path + 'time_series_covid19_deaths_US.csv')
+
+uid = us_deaths.columns[0]
+dates = us_deaths.columns[12:]
