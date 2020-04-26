9a655148b50f4b6d4cfeb1a7db95e4e51ae4ac18
Zach Smith <zsmith126028@gmail.com>
2020-04-26 12:46:36 -0500

    get necessary dates from datetime in order to add sql columns

--- dataParse/recent_data.py
+++ dataParse/recent_data.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import mysql.connector
 import os
+import datetime
 
 csv_path = '/home/zach/Storage/Projects/COVID-19/covidData/csse_covid_19_data/csse_covid_19_time_series/'
 
 us_deaths = pd.read_csv(csv_path + 'time_series_covid19_deaths_US.csv')
+us_confirmed = pd.read_csv(csv_path + 'time_series_covid19_confirmed_US.csv')
 
 # Create database connection
 mydb = mysql.connector.connect(
@@ -15,3 +17,21 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
+
+recent_column = us_deaths.columns[-1]
+
+# for item in us_deaths[recent_column][:-1]:
+  # print(item)
+
+todays_date = datetime.date.today()
+# print(todays_date.year)
+current_year = todays_date.year
+current_month = todays_date.month
+current_day =  todays_date.day
+
+yesterday = str(current_month) + "/" + str(current_day - 1) + "/" + str(current_year)
+two_days_prior = str(current_month) + "/" + str(current_day - 2) + "/" + str(current_year)
+print(yesterday)
+print(two_days_prior)
+
+# def get_recent_data(recent_column_date, yesterday, data):
