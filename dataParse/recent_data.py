3dc9605162f5e86c658b08138d55c0bb14d99d94
Zach Smith <zsmith126028@gmail.com>
2020-04-26 15:10:54 -0500

    remove info related to datetime. create id array

--- dataParse/recent_data.py
+++ dataParse/recent_data.py
@@ -2,7 +2,6 @@ import pandas as pd
 from sqlalchemy import create_engine
 import mysql.connector
 import os
-import datetime
 
 csv_path = '/home/zach/Storage/Projects/COVID-19/covidData/csse_covid_19_data/csse_covid_19_time_series/'
 
@@ -12,24 +11,16 @@ us_confirmed = pd.read_csv(csv_path + 'time_series_covid19_confirmed_US.csv')
 # Create database connection
 engine = create_engine(f"mysql+mysqlconnector://zach:{os.environ['DBPASSWD']}@127.0.0.1:3306/covid19")
 
-print(engine)
+# print(engine)
 
-todays_date = datetime.date.today()
-# print(todays_date.year)
-current_year = todays_date.year
-current_month = todays_date.month
-current_day =  todays_date.day
+# print(us_deaths)
 
-yesterday = str(current_month) + "/" + str(current_day - 1) + "/" + str(current_year)[2:]
-two_days_prior = str(current_month) + "/" + str(current_day - 2) + "/" + str(current_year)[2:]
-# print(type(yesterday))
-# print(yesterday)
-# print(type(two_days_prior))
-# print(two_days_prior)
+uid_arr = []
+increment = 1
 
-'''
-There could be an issue with the way the dates are created.
-The subtraction from the current date could (probably will) 
-cause an issue when the month changes. Not sure yet.
-'''
+# create ids for new csv
+while increment < 3262:
+  uid_arr.append(increment)
+  increment += 1
+
 
