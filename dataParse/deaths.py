1f7e189de8a3ee39266abd2924d824358bac587e
Zach Smith <zsmith126028@gmail.com>
2020-04-26 15:15:57 -0500

    remove commented code that is not needed

--- dataParse/deaths.py
+++ dataParse/deaths.py
@@ -20,26 +20,7 @@ mycursor = mydb.cursor()
 
 mycursor.execute("CREATE TABLE IF NOT EXISTS deaths (UID INT AUTO_INCREMENT PRIMARY KEY)")
 
-# for item in dates:
-#   mycursor.execute(f"ALTER TABLE deaths ADD COLUMN `{item}` INT FIRST")
-
 # reverse array in order to add items in sql table in proper order
 for item in reversed(dates):
   mycursor.execute(f"ALTER TABLE deaths ADD COLUMN `{item}` INT AFTER UID")
 
-# for item in dates:
-#   for num in us_deaths[item]:
-#     print(item, num)
-
-# deaths_arr = []
-
-
-# for item in dates:
-  # sql = f"INSERT INTO deaths (`{item}`) VALUES (%s)"
-  # data_arr = list(zip(us_deaths[item][:-1]))
-  # deaths_arr.append(data_arr)
-
-# mycursor.executemany(sql, deaths_arr)
-# print(deaths_arr[-1])
-
-# mydb.commit()
