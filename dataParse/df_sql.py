1eb5a2e79bfa6494355b4c65fceb972273d98643
darlose04 <zsmith126028@gmail.com>
2020-05-01 15:27:23 -0500

    push dataframe from csv into sql tables

--- dataParse/df_sql.py
+++ dataParse/df_sql.py
@@ -2,3 +2,27 @@ import pandas as pd
 from sqlalchemy import create_engine
 import mysql.connector
 import os
+
+csv_path = "/home/zach/Storage/Projects/COVID-19/"
+
+deaths = pd.read_csv(csv_path + 'deaths.csv')
+cases = pd.read_csv(csv_path + 'cases.csv')
+
+# delete first column that was created automatically when csv was created
+del deaths['Unnamed: 0']
+del cases['Unnamed: 0']
+
+# Create database connection
+# mydb = mysql.connector.connect(
+#   host='localhost',
+#   user='zach',
+#   passwd=os.environ['DBPASSWD'],
+#   database='covid19'
+# )
+
+# mycursor = mydb.cursor()
+
+engine = create_engine(f"mysql+mysqlconnector://zach:{os.environ['DBPASSWD']}@127.0.0.1:3306/covid19")
+
+deaths.to_sql('deaths', engine, if_exists='replace', index=True, index_label=None, method=None)
+cases.to_sql('cases', engine, if_exists='replace', index=True, index_label=None, method=None)
