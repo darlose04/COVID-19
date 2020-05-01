522d18ed543f870601f2386759672303f947e7b4
darlose04 <zsmith126028@gmail.com>
2020-05-01 15:31:30 -0500

    remove commented code that is no longer needed

--- dataParse/df_sql.py
+++ dataParse/df_sql.py
@@ -12,16 +12,6 @@ cases = pd.read_csv(csv_path + 'cases.csv')
 del deaths['Unnamed: 0']
 del cases['Unnamed: 0']
 
-# Create database connection
-# mydb = mysql.connector.connect(
-#   host='localhost',
-#   user='zach',
-#   passwd=os.environ['DBPASSWD'],
-#   database='covid19'
-# )
-
-# mycursor = mydb.cursor()
-
 engine = create_engine(f"mysql+mysqlconnector://zach:{os.environ['DBPASSWD']}@127.0.0.1:3306/covid19")
 
 deaths.to_sql('deaths', engine, if_exists='replace', index=True, index_label=None, method=None)
