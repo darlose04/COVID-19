d97207ecc374d76fedaa56d8aeabdc39b6e4db00
darlose04 <zsmith126028@gmail.com>
2020-05-07 13:16:05 -0500

    remove seaborn, just using matplotlib

--- dataParse/data_vis.py
+++ dataParse/data_vis.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 csv_path = '/home/zach/Storage/Projects/COVID-19/covidData/csse_covid_19_data/csse_covid_19_time_series/'
 
@@ -14,6 +13,8 @@ confirmed_dates = us_confirmed.columns[11:]
 total_deaths = []
 total_cases = []
 
+
+
 for column in death_dates:
   total_deaths.append(us_deaths[column].sum())
 
@@ -24,9 +25,20 @@ for column in confirmed_dates:
 # print(total_cases)
 
 # Matplotlib line chart
-# plt.plot(death_dates, total_deaths)
+# plt.plot(confirmed_dates, total_cases)
 # plt.xlabel("Dates")
-# plt.ylabel("Deaths")
-# plt.title("COVID-19 Deaths U.S.")
-# plt.show()
+# plt.ylabel("Cases")
+# plt.title("COVID-19 Cases U.S.")
 
+case_and_deaths = list(zip(total_cases, total_deaths))
+
+cases_df = pd.DataFrame(dict(dates=confirmed_dates, cases=total_cases))
+# print(cases_df)
+
+plt.plot(confirmed_dates, case_and_deaths)
+plt.xlabel("Dates")
+plt.ylabel("Cases and Deaths")
+
+
+
+plt.show()
