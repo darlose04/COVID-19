1c09961bc2183158d235bd3a7a85f5251de28dad
darlose04 <zsmith126028@gmail.com>
2020-05-07 11:45:26 -0500

    add access to csv data

--- dataParse/data_vis.py
+++ dataParse/data_vis.py
@@ -2,3 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+csv_path = '/home/zach/Storage/Projects/COVID-19/covidData/csse_covid_19_data/csse_covid_19_time_series/'
+
+us_deaths = pd.read_csv(csv_path + 'time_series_covid19_deaths_US.csv')
+us_confirmed = pd.read_csv(csv_path + 'time_series_covid19_confirmed_US.csv')
