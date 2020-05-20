c364708561e0f8b72801d9c0266ea46983146471
darlose04 <zsmith126028@gmail.com>
2020-05-20 10:13:14 -0500

    add bash script to run python scripts

--- runPython.sh
+++ runPython.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-echo "Running Python data scripts at $(date +%B%d,%T)" > /home/zach/Storage/Projects/COVID-19/runPython.out
 
-python3 /home/zach/Storage/Projects/COVID-19/dataParse/deaths_us.py
+echo "Running Python scripts to update the database at $(date +%B%d,%T)" > /home/zach/Storage/Projects/COVID-19/pythonScripts.out
 
-echo "Scripts done running " >> /home/zach/Storage/Projects/COVID-19/runPython.out
+echo "Running recent_data.py to create and update the CSVs" >> /home/zach/Storage/Projects/COVID-19/pythonScripts.out
+python3 /home/zach/Storage/Projects/COVID-19/dataParse/recent_data.py
+echo "Done running recent_data.py" >> /home/zach/Storage/Projects/COVID-19/pythonScripts.out
+
+echo "Running df_sql.py to push newly created CSVs into local MySQL database" >> /home/zach/Storage/Projects/COVID-19/pythonScripts.out
+python3 /home/zach/Storage/Projects/COVID-19/dataParse/df_sql.py
+echo "Done running df_sql.py" >> /home/zach/Storage/Projects/COVID-19/pythonScripts.out
+
+echo "Running daily_report.py to add the daily report CSV to the database" >> /home/zach/Storage/Projects/COVID-19/pythonScripts.out
+python3 /home/zach/Storage/Projects/COVID-19/dataParse/daily_report.py
+echo "Done running daily_report.py" >> /home/zach/Storage/Projects/COVID-19/pythonScripts.out
+
+echo "Python scripts finished" >> /home/zach/Storage/Projects/COVID-19/pythonScripts.out
