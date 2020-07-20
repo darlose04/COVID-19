7fd35fc33ccf878eadb11643a909ba567c76ec8b
darlose04 <zsmith126028@gmail.com>
2020-07-20 11:32:40 -0500

    change rows used since a row was added

--- dataParse/recent_data.py
+++ dataParse/recent_data.py
@@ -12,8 +12,8 @@ csv_path = '/home/zach/Storage/Projects/COVID-19/covidData/csse_covid_19_data/cs
 us_deaths = pd.read_csv(csv_path + 'time_series_covid19_deaths_US.csv')
 us_confirmed = pd.read_csv(csv_path + 'time_series_covid19_confirmed_US.csv')
 
-death_dates = us_deaths.columns[12:]
-confirmed_dates = us_confirmed.columns[11:]
+death_dates = us_deaths.columns[13:]
+confirmed_dates = us_confirmed.columns[12:]
 
 def create_csv(data, dates, csv_name):
   columns = ['UID']
