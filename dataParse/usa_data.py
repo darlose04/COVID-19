4fb78b63546fb1d10995d913552085fb55a13633
Zach Smith <zsmith126028@gmail.com>
2020-07-29 10:03:17 -0500

    python linter changed tab sizes

--- dataParse/usa_data.py
+++ dataParse/usa_data.py
@@ -7,20 +7,20 @@ csv_path = '/home/zach/Storage/Projects/COVID-19/covidData/csse_covid_19_data/cs
 us_deaths = pd.read_csv(csv_path + 'time_series_covid19_deaths_US.csv')
 
 mydb = mysql.connector.connect(
-  host='localhost',
-  user='zach',
-  passwd=os.environ['DBPASSWD'],
-  database='covid19'
+    host='localhost',
+    user='zach',
+    passwd=os.environ['DBPASSWD'],
+    database='covid19'
 )
 
 # Make variables for each column from the csv that I want to push into SQL
 
-uid = us_deaths.columns[0] # int or float
+uid = us_deaths.columns[0]  # int or float
 city = us_deaths.columns[5]  # string. will need to account for null values
-province_state = us_deaths.columns[6] # string
-latitude = us_deaths.columns[8] # float
-longitude = us_deaths.columns[9] # float
-population = us_deaths.columns[11] # int
+province_state = us_deaths.columns[6]  # string
+latitude = us_deaths.columns[8]  # float
+longitude = us_deaths.columns[9]  # float
+population = us_deaths.columns[11]  # int
 
 # want to grab every item in columns except for last row since last row is NaN
 # Last row also doesn't have useful info
@@ -41,12 +41,13 @@ sql = "INSERT INTO usa (City, State, Latitude, Longitude, Population) VALUES (%s
 city_arr = []
 
 for item in city_vals:
-  if item != item:
-    city_arr.append(None)
-  else:
-    city_arr.append(item)
+    if item != item:
+        city_arr.append(None)
+    else:
+        city_arr.append(item)
 
-data_arr = list(zip(city_arr, province_state_vals, latitude_vals, longitude_vals, population_vals))
+data_arr = list(zip(city_arr, province_state_vals,
+                    latitude_vals, longitude_vals, population_vals))
 
 mycursor.executemany(sql, data_arr)
 mydb.commit()
