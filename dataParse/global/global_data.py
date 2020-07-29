8286283cd8511b729deee7d79c3cbac0fd95f4d9
Zach Smith <zsmith126028@gmail.com>
2020-07-29 10:16:35 -0500

    create global table for province, country, latitude, and longitude values

--- dataParse/global/global_data.py
+++ dataParse/global/global_data.py
@@ -3,3 +3,45 @@ import mysql.connector
 import os
 
 csv_path = '/home/zach/Storage/Projects/COVID-19/covidData/csse_covid_19_data/csse_covid_19_time_series/'
+
+global_cases = pd.read_csv(
+    csv_path + 'time_series_covid19_confirmed_global.csv')
+
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='zach',
+    passwd=os.environ['DBPASSWD'],
+    database='covid19'
+)
+
+# Make variables for each column from the csv that I want to push into SQL
+province = global_cases.columns[0]
+country = global_cases.columns[1]
+latitude = global_cases.columns[2]
+longitude = global_cases.columns[3]
+
+province_vals = global_cases[province]
+country_vals = global_cases[country]
+latitude_vals = global_cases[latitude]
+longitude_vals = global_cases[longitude]
+
+province_arr = []
+
+for item in province_vals:
+    if item != item:
+        province_arr.append(None)
+    else:
+        province_arr.append(item)
+
+# create db table
+mycursor = mydb.cursor()
+mycursor.execute(
+    "CREATE TABLE global (ID INT AUTO_INCREMENT PRIMARY KEY, Province VARCHAR(255), Country VARCHAR(255), Latitude FLOAT(10, 8), Longitude FLOAT(11, 8))")
+
+# inserting values into the table
+sql = "INSERT INTO global (Province, Country, Latitude, Longitude) VALUES (%s, %s, %s, %s)"
+
+data_arr = list(zip(province_arr, country_vals, latitude_vals, longitude_vals))
+
+mycursor.executemany(sql, data_arr)
+mydb.commit()
