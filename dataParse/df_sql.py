87459f7ebe71bc8deb365c1a9c22076788d1853a
Zach Smith <zsmith126028@gmail.com>
2020-07-29 11:03:24 -0500

    add global data csvs to sql

--- dataParse/df_sql.py
+++ dataParse/df_sql.py
@@ -7,10 +7,14 @@ csv_path = "/home/zach/Storage/Projects/COVID-19/"
 
 deaths = pd.read_csv(csv_path + 'deaths.csv')
 cases = pd.read_csv(csv_path + 'cases.csv')
+global_cases = pd.read_csv(csv_path + 'dataParse/global/global_cases.csv')
+global_deaths = pd.read_csv(csv_path + 'dataParse/global/global_deaths.csv')
 
 # delete first column that was created automatically when csv was created
 del deaths['Unnamed: 0']
 del cases['Unnamed: 0']
+del global_cases['Unnamed: 0']
+del global_deaths['Unnamed: 0']
 
 engine = create_engine(
     f"mysql+mysqlconnector://zach:{os.environ['DBPASSWD']}@localhost:3306/covid19")
@@ -19,3 +23,7 @@ deaths.to_sql('deaths', engine, if_exists='replace',
               index=True, index_label=None, method=None)
 cases.to_sql('cases', engine, if_exists='replace',
              index=True, index_label=None, method=None)
+global_cases.to_sql('global_cases', engine, if_exists='replace',
+                    index=True, index_label=None, method=None)
+global_deaths.to_sql('global_deaths', engine, if_exists='replace',
+                     index=True, index_label=None, method=None)
