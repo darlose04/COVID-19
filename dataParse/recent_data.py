eafcbcf4dc8f2f5d9ffa4294a10a531aa911c39c
Zach Smith <zsmith126028@gmail.com>
2020-10-12 13:05:13 -0500

    change path of csv files

--- dataParse/recent_data.py
+++ dataParse/recent_data.py
@@ -15,43 +15,42 @@ us_confirmed = pd.read_csv(csv_path + 'time_series_covid19_confirmed_US.csv')
 death_dates = us_deaths.columns[12:]
 confirmed_dates = us_confirmed.columns[11:]
 
+
 def create_csv(data, dates, csv_name):
-  columns = ['UID']
-
-  for header in dates:
-    columns.append(header)
-
-  uid_arr = []
-  increment = 1
-
-  # create ids for new csv
-  while increment < 3341:
-    uid_arr.append(increment)
-    increment += 1
-
-  count = 1
-  col_arr = []
-
-  while count < 3341:
-    sub_arr = []
-
-    sub_arr.append(uid_arr.pop(0))
+    columns = ['UID']
 
     for header in dates:
-      sub_arr.append(data[header].pop(count-1))
+        columns.append(header)
 
+    uid_arr = []
+    increment = 1
 
-    col_arr.append(sub_arr)
+    # create ids for new csv
+    while increment < 3341:
+        uid_arr.append(increment)
+        increment += 1
 
-    count += 1
-  
-  csv = csv_name + '.csv'
+    count = 1
+    col_arr = []
 
-  csv_name = pd.DataFrame(col_arr, columns=columns,)
-  csv_name.to_csv(csv)
+    while count < 3341:
+        sub_arr = []
+
+        sub_arr.append(uid_arr.pop(0))
+
+        for header in dates:
+            sub_arr.append(data[header].pop(count-1))
+
+        col_arr.append(sub_arr)
+
+        count += 1
+
+    csv = csv_name + '.csv'
+
+    csv_name = pd.DataFrame(col_arr, columns=columns,)
+    csv_name.to_csv(csv)
 
 
 # print(us_deaths['4/30/20'])
 create_csv(us_deaths, death_dates, 'deaths')
 create_csv(us_confirmed, confirmed_dates, 'cases')
-
