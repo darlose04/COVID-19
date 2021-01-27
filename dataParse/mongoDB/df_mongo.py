767341a0947c068dfe62f3970a207591d64328d7
Zach Smith <zsmith126028@gmail.com>
2021-01-27 15:23:01 -0600

    figuring out structure of dict to push into mongodb. trying different pandas methods to sort info properly

--- dataParse/mongoDB/df_mongo.py
+++ dataParse/mongoDB/df_mongo.py
@@ -1,3 +1,56 @@
 import pandas as pd
 import os
 from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017/")
+
+csv_path = "/home/zach/Storage/Projects/COVID-19/covidData/csse_covid_19_data/csse_covid_19_time_series/"
+
+us_confirmed = pd.read_csv(csv_path + "time_series_covid19_confirmed_US.csv")
+us_deaths = pd.read_csv(csv_path + "time_series_covid19_deaths_US.csv")
+
+db = client.covid19
+collection = db.usa
+
+states = list(set(us_confirmed["Province_State"]))
+states.sort()
+# print(states)
+
+counties = us_confirmed["Admin2"]
+
+# print(us_confirmed.iloc[0, 0:len(us_confirmed)])
+
+one_row = us_confirmed.iloc[0, 0:len(us_confirmed)]
+# print(one_row[:20])
+
+for item in one_row:
+    print(item)
+
+us_data = {
+    'states': [
+        {
+            'state_name': "",
+            'counties': [
+                {
+                    'county_name': "",
+                    'cases': [
+                        {
+                          'date': 0
+                        }
+                    ],
+                    'deaths': [
+                        {
+                            'date': 0
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            'state_name': "",
+            'counties': []
+        }
+    ]
+}
+
+# print(us_data['states'][0]['state_name'])
